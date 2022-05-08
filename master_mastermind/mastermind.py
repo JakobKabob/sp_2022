d60a0b20df540d293411de81cbc4cd74ed67654c
jakobkabob <jakobkabob@kabobaboba.bob>
2022-05-08 23:45:55 +0200

    working structure

--- master_mastermind/mastermind.py
+++ master_mastermind/mastermind.py
@@ -3,6 +3,8 @@ import sys
 import string
 import random
 import getopt
+import time
+from pprint import pprint
 
 DOT_FILLED = '\u25CF'
 DOT_EMPTY = '\u25CB'
@@ -26,7 +28,7 @@ def update_ui(state: list, options, solution=None):
         formatted_sol = ' '.join([*solution])
     else:
         formatted_sol = ' '.join([*'_'*len(state[0][1])])
-    print(f'sol: \t{formatted_sol}\t\toptions: {options}')
+    print(f'sol: \t{formatted_sol}\t\tset: {options}')
 
     formatted_state = ''
     for round in state[::-1]:
@@ -40,7 +42,6 @@ def update_ui(state: list, options, solution=None):
     print(formatted_state+f'\x1B[{len(state)+2}F')
 
 
-    
 def set_feedback(state: list, solution) -> bool:
     """
     Changes updates feedback of state, returns bool which indicates a win.
@@ -60,7 +61,7 @@ def set_feedback(state: list, solution) -> bool:
                 right_place.append(round[1][i]) 
                 if round[1][i] in wrong_place:
                     wrong_place.remove(round[1][i])
-            elif round[1][i] in solution and round[1][i] not in wrong_place:
+            elif round[1][i] in solution and round[1][i] not in wrong_place+right_place:
                 wrong_place.append(round[1][i]) 
                 if round[1][i] in right_place:
                     right_place.remove(round[1][i])
@@ -69,6 +70,8 @@ def set_feedback(state: list, solution) -> bool:
         feedback += [NO_DOT for _ in range(len(solution)-(len(wrong_place)+len(right_place)))]
 
         round[2] = feedback
+        with open('state.txt', 'w') as file:
+            pprint(state, stream=file)
         if len(right_place) == len(solution):
             return True
 
@@ -92,6 +95,8 @@ def mainloop_human(state: list, options: list, solution: list):
     solution (list): solution to be checked against
     options (list): options from which a user can choose
     """
+    # the ui overwrites itself with every round, based on the shared state, this means that the cursor needs to be 
+    # in the very same spot every round.
     for round_number in range(len(state)):
 
         while True:
@@ -108,20 +113,67 @@ def mainloop_human(state: list, options: list, solution: list):
 
         state[round_number][1] = [*guess.upper()]
         if set_feedback(state, solution):
-            break
+            update_ui(state, options=options, solution=solution)
+            print(f'\x1B[{len(state)+1}B')
+            print('you won!')
+            exit(0)
 
     update_ui(state, options=options, solution=solution)
     print(f'\x1B[{len(state)+1}B')
+    print('u lost')
+
+
+def mainloop_simple_algo(state: list, options: list, solution: list):
+    """
+    Mainloop simple algorithm.
+
+    Parameters:
+    state (list): input state
+    solution (list): solution to be checked against
+    options (list): options from which a user can choose
+    """
+    for round_number in range(len(state)):
+
+        update_ui(state, options=options)
+        guess = random.sample(options, len(state[0][1])) 
+
+        state[round_number][1] = guess
+        if set_feedback(state, solution):
+            update_ui(state, options=options, solution=solution)
+            print(f'\x1B[{len(state)+1}B')
+            print('you lost!')
+            exit(0)
+
+        time.sleep(1)
+
+    update_ui(state, options=options, solution=solution)
+    print(f'\x1B[{len(state)+1}B')
+    print('you won!')
 
 
 def usage():
-    print('iuwbrg')
+    title = 'Mastermind implementation Structured Programming 2022 - Jochem Boerma\n'
+    text = 'This implementation of mastermind has two distinct modes: \"choose\" and \"guess\", when using \
+            \nwithout any options the gamemode \"guess\" will be assumed with a solution length of 4 and 6 \
+            \ndistinct values to choose from. \
+            \nIf you want to play in \"choose\" mode you have to use the --solution [STRING] option. \
+            \n\nAlso, this implementation uses letters instead of colours\n'
+    
+    options = ['-p --positions[NUMBER]: default=4',
+        '-v --values[NUMBER] default=6',
+        '-r --rounds[NUMBER] default=10',
+        '-s --solution[STRING]: implicit \"choose\" mode setting',
+        '-a --algo[STRING]: decide on the algorithm the computer will use while in \"choose\" mode, default is \"simple\"' ]
+
+    print(title)
+    print(text)
+    print('options:\n\t' + '\n\t'.join(options))
 
 
 def main():
     argv = sys.argv[1:]
     try:
-        opts, _ = getopt.getopt(argv, "s:t:w:h", ["algo=", "width=","help"])
+        opts, _ = getopt.getopt(argv, "a:s:v:p:r:h", ["algo=", "solution=", "values=", "positions=", "rounds=","help"])
     except Exception as exp:
         raise Exception(exp)
 
@@ -130,46 +182,52 @@ def main():
     solution = None
     options = None
     algo = None
+    set_count = None
 
     for opt, arg in opts:
-        if opt in ['-s']:
+        if opt in ['-s', '--solution']:
             solution = list(arg.upper())
             if len(set(solution)) != len(solution):
                 raise Exception('Solution must not contain double characters')
             option_places = len(solution)
-        elif opt in ['-t']:
+        elif opt in ['-r', '--rounds']:
             tries = int(arg)
-        elif opt in ['-w', '--width']:
+        elif opt in ['-p', '--positions']:
             if solution is None:
                 if int(arg) > 12:
                     raise Exception(f'Max {opt} is 12')
                 option_places = int(arg)
             else: 
                 print('ignoring \'{opt}\' for \'-s\' is already specified')
-        elif opt in ['--algo']:
+        elif opt in ['-a', '--algo']:
             algo = int(arg)
+        elif opt in ['-v', '--values']:
+            set_count = int(arg)
         elif opt in ('-h', '--help'):
             usage()
             sys.exit()
 
 
     if solution is None:
-        options = random.sample(string.ascii_uppercase, option_places*2)
+        options = random.sample(string.ascii_uppercase, (option_places*2) if set_count is None else set_count)
         solution = random.sample(options, option_places)
     else:
-        options = random.sample([x for x in string.ascii_uppercase if x not in solution], option_places)
+        if len(set(solution)) != len(solution):
+            raise Exception('no support for duplicates in solution!')
+        options = random.sample([x for x in string.ascii_uppercase if x not in solution], (option_places*2) if set_count is None else set_count)
         options+=solution
     options.sort()
 
     state = [[nr, [*'_'*option_places], [*NO_DOT*option_places]] for nr in [*range(1, tries+1)]]
 
-    if algo == 1:
-        pass
-    elif algo == 2:
-        pass
-    else:
+    if solution is None:
         mainloop_human(state, options, solution)
-
+    elif algo is None or algo == "simple":
+        print(f'You are playing against "simple"')
+        mainloop_simple_algo(state, options, solution)
+    else:
+        raise Exception(f'bruh no algo called {algo}')
 
 if __name__ == "__main__":
+    print('(first time? Use python mastermind.py --help)')
     main()
